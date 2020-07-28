2198e15acc391bdb624bc40b2e785e0776c6f15c
Johnny Briones <jbrrii91@gmail.com>
2020-07-28 14:15:22 -0400

    add time and date

--- main.mjs
+++ main.mjs
@@ -1,20 +1,32 @@
 // Sets the date
-// var today = new Date();
-// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-// console.log(today);
-
+var today = new Date();
+var date = (today.getMonth()+1) + '-' + today.getDate() + '-' + today.getFullYear();
+console.log(today);
+document.all[10].textContent = date;
 
 // Sets the time
-// var today = new Date();
-// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// console.log(time);
-
 var today = new Date();
-
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+console.log(time);
+document.all[12].textContent = time;
 
-var dateTime = date +' '+ time;
+// Sets the date AND time
+// var today = new Date();
+// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var dateTime = date +' '+ time;
+// console.log(dateTime);
 
-console.log(dateTime);
+// You can not use for a timestamp by linking this 
+// with an action and writing it in a log file.
+
+// Examine The Document Object
+// console.dir(document);
+
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.all);
+console.log(document.all[10]);
