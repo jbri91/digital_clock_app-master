83dcd52230fc3f5c6c835ac298c5bdd7708746ce
Johnny Briones <jbrrii91@gmail.com>
2020-07-28 16:04:47 -0400

    working with a function

--- main.mjs
+++ main.mjs
@@ -1,32 +1,21 @@
+// let i = 0;
+
+// function increment() {
+//   i++;
+//   console.log(i);
+// }
+
+// setInterval(increment, 1000);
+
 // Sets the date
 var today = new Date();
 var date = (today.getMonth()+1) + '-' + today.getDate() + '-' + today.getFullYear();
 console.log(today);
 document.all[10].textContent = date;
+// var dates = document.getElementsByClassName('date');
+// // console.log(dates);
 
 // Sets the time
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-console.log(time);
 document.all[12].textContent = time;
-
-// Sets the date AND time
-// var today = new Date();
-// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// var dateTime = date +' '+ time;
-// console.log(dateTime);
-
-// You can not use for a timestamp by linking this 
-// with an action and writing it in a log file.
-
-// Examine The Document Object
-// console.dir(document);
-
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.all);
-console.log(document.all[10]);
