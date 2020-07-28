222df897bf903cea0ddff3b9d9917b1649efba5c
Johnny Briones <jbrrii91@gmail.com>
2020-07-28 13:46:04 -0400

    I added time and date in JS

--- main.mjs
+++ main.mjs
@@ -1,3 +1,20 @@
-// Testing comments and pushing into GitHub.
-//Update, I was not able to push or pull using the command line
-//Ask how to do so on next call
+// Sets the date
+// var today = new Date();
+// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+// console.log(today);
+
+
+// Sets the time
+// var today = new Date();
+// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// console.log(time);
+
+var today = new Date();
+
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+var dateTime = date +' '+ time;
+
+console.log(dateTime);
