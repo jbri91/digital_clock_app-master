54980635dc9c55f499146a76b1514a29251f5578
Johnny Briones <jbrrii91@gmail.com>
2020-07-29 20:14:50 -0400

    got the clock to tick

--- main.mjs
+++ main.mjs
@@ -4,15 +4,26 @@ var today = new Date();
 var date = (today.getMonth()+1) + '-' + today.getDate() + '-' + today.getFullYear();
 console.log(today);
 document.all[10].textContent = date;
-// var dates = document.getElementsByClassName('date');
-// // console.log(dates);
+
+let i = 0;
+
+function increment() {
+  i++;
+  console.log(i);
+}
+
+setInterval(increment, 1000);
+
 
 
 // Sets the time
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+function increment() {
+today = new Date();
+time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 document.all[12].textContent = time;
 console.log(time);
+}
+
 
 
 // let i = 0;
