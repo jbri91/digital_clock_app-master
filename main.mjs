ff769b4a1e451cfde42114abd6ac705a52dbddfe
Johnny Briones <jbrrii91@gmail.com>
2020-07-31 20:28:41 -0400

    added two digit time

--- main.mjs
+++ main.mjs
@@ -1,42 +1,48 @@
-// Global Variable
+// // Global Variable
 militaryTime = false;
-
-
-// Sets the date
 var today = new Date();
-var date = (today.getMonth()+1) + '-' + today.getDate() + '-' + today.getFullYear();
-document.getElementById('current_date').innerHTML = date;
 
-function standardTime() {
-today = new Date();
-hours = today.getHours();
-if (hours < 0) {
-    hours + '1';
-}
-}
+
+
+// // Sets the date\
+var date = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
+document.getElementById('current_date').innerHTML = date;
 
 
 
 function increment() {
-  today = new Date();  
+    hours = today.getHours();
+    minutes = today.getMinutes();
+    seconds = today.getSeconds();
+    today = new Date();
+if (hours <10) {
+    hours = '0' + hours;
+}
+if (minutes < 10) {
+    minutes = '0' + minutes;
+}
+if (seconds < 10) {
+    seconds = '0' + seconds;
+}
 if (militaryTime == true) {
-    time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    time = hours + ":" + minutes + ":" + seconds;
     document.getElementById('current_time').innerHTML = time;
 }
     else {
-    time = today.getHours() - 12 + ":" + today.getMinutes() + ":" + today.getSeconds();
+    time = (hours -12) + ":" + minutes + ":" + seconds;
     document.getElementById('current_time').innerHTML = time;
-}
-}
+} }
 
 
-// Sets the interval at 1000 ms
+// // Sets the interval at 1000 ms
 setInterval(increment, 1000);
 
-// Links to HTML button file
+// // Links to HTML button file
 const toggle = document.getElementById('toggle')
 
-// This is toggling the on and off for military vs standard time
+// // This is toggling the on and off for military vs standard time
 toggle.addEventListener('click', function() {
     militaryTime = !militaryTime;
     });
+
+
