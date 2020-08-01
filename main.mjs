c8a2e78ada1627644cea9bca726780d279b90a04
Johnny Briones <jbrrii91@gmail.com>
2020-08-01 13:05:24 -0400

    time is good

--- main.mjs
+++ main.mjs
@@ -5,43 +5,42 @@ var today = new Date();
 
 
 // // Sets the date\
-var date = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
+var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
 document.getElementById('current_date').innerHTML = date;
 
 
 
 function increment() {
-    hours = today.getHours();
-    minutes = today.getMinutes();
-    seconds = today.getSeconds();
-    today = new Date();
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    
 
-// if (hours < 0) {
-//     hours = Math.abs(hours);       I tried to prevent a negative number from appearing.
-// }
-if (hours <10) {
-    hours = '0' + hours;
-}
-if (hours = 0) {
-    hours = 0 + 1;                 /* I am trying to prevent the hours from being 0.*/
-}
-if (minutes < 10) {
-    minutes = '0' + minutes;
-}
-if (seconds < 10) {
-    seconds = '0' + seconds;
-}
-if (militaryTime == true) {
-    time = hours + ":" + minutes + ":" + seconds;
-    document.getElementById('current_time').innerHTML = time;
-}
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (hours == 0) {
+        hours = 12;                
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+    if (militaryTime == true) {
+        time = hours + ":" + minutes + ":" + seconds;
+        document.getElementById('current_time').innerHTML = time;
+    }
     else {
-    time = (hours -12) + ":" + minutes + ":" + seconds;
-    document.getElementById('current_time').innerHTML = time;
-} /*I am trying to prevent a negative number from appearing when it is 11 military time */
-if (militaryTime == false && hours < 0) {       
-    hours = Math.abs(hours);
-}
+        if (hours > 12) {
+            hours = hours - 12
+        }
+        time = (hours) + ":" + minutes + ":" + seconds;
+        document.getElementById('current_time').innerHTML = time;
+    } 
 
 }
 
@@ -53,9 +52,9 @@ setInterval(increment, 1000);
 const toggle = document.getElementById('toggle')
 
 // // This is toggling the on and off for military vs standard time
-toggle.addEventListener('click', function() {
+toggle.addEventListener('click', function () {
     militaryTime = !militaryTime;
-    });
+});
 
 
 
